56a737699bb3e6600c92622b9cb27cb7e9ea5fff
Nikita Malov <nikita.volam@gmail.com>
2023-02-04 10:43:39 +0200

    Add Spring Boot dependecies

---
+++ src/main/java/com/nikitascompany/TelegramUnBanBot/TelegramUnBanBotApplication.java
@@ -0,0 +1,13 @@
+package com.nikitascompany.TelegramUnBanBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramUnBanBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramUnBanBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikitascompany/TelegramUnBanBot/TelegramUnBanBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikitascompany.TelegramUnBanBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramUnBanBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
